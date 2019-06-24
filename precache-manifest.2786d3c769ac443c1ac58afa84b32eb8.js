0fe18ec2dc2376dfef1d80c804a75e744dc00792
Yash Naik <yashnaik47@gmail.com>
2019-06-24 23:32:36 +0530

    Updates

---
+++ precache-manifest.2786d3c769ac443c1ac58afa84b32eb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05414e258212a67932afed04d20d448d",
+    "url": "/react-gh-repo/index.html"
+  },
+  {
+    "revision": "35c005ff16f4c39e2203",
+    "url": "/react-gh-repo/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/react-gh-repo/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "35c005ff16f4c39e2203",
+    "url": "/react-gh-repo/static/js/main.60324d12.chunk.js"
+  },
+  {
+    "revision": "3b632474a907a00183d6",
+    "url": "/react-gh-repo/static/js/runtime~main.f1cc09f0.js"
+  }
+]);
